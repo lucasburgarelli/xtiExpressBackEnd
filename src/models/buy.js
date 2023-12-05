a603a1c616a3d528175b852809032bda94bb907d
souzannatha <souzanatha04@gmail.com>
2023-12-05 07:54:50 -0300

    Add 'Buy' model and basic CRUD operations

--- src/models/buy.js
+++ src/models/buy.js
@@ -1,2 +1,18 @@
 const { DataTypes, Op } = require("sequelize");
 const sequelize = require("../helpers/connection");
+
+const BuyModel = sequelize.define("Buy", {
+  use_cpf: {},
+  sto_code: {},
+  buy_amount: {},
+  buy_mediumprice: {},
+  buy_date: {},
+  buy_time: {},
+});
+
+module.exports = {
+  create: async function (buy) {},
+  read: async function () {},
+  update: async function (cpf, code, date, time) {},
+  delete: async function (cpf, code, date, time) {},
+};
