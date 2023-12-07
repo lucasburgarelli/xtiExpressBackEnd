175d9a97e16bdd61f517f67c717117496f6305bc
lucasburgarelli <lucasburgarellim@gmail.com>
2023-12-07 19:22:26 -0300

    Stock controller methods converted to async

--- src/controllers/stock-controller.js
+++ src/controllers/stock-controller.js
@@ -4,12 +4,13 @@ const {sucess, fail} = require("../helpers/response")
 
 
 
-exports.get = (req, res, next) => {
-    res.status(200).json(sucess(StockModel.read()))
+exports.get = async (req, res, next) => {
+    let stocks = await StockModel.read()
+    res.status(200).json(sucess(stocks))
 }
 
-exports.getByCode = (req, res, next) => {
-    const stock = StockModel.read()
+exports.getByCode = async (req, res, next) => {
+    const stock = await StockModel.read()
     if(!stock) res.status(404).json(fail(stock))
     res.status(200).json(sucess(stock))
 }
