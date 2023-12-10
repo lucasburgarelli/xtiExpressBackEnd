583c57a49f9c631c0f65eebbc70196a78ed77693
Nathã Souza Lopes <souzanatha04@gmail.com>
2023-12-10 19:42:28 -0300

    Buy and User Controllers updated.

--- src/controllers/buy-controller.js
+++ src/controllers/buy-controller.js
@@ -2,47 +2,73 @@ const BuyModel = require("../models/buy");
 const { sucess, fail } = require("../helpers/response");
 
 exports.post = async (req, res, next) => {
-  let buy = await BuyModel.create(req.body);
-  if (!buy) res.status(403).json(fail(buy));
-  else res.status(200).json(sucess(buy));
+  try {
+    let buy = await BuyModel.create(req.body);
+    if (!buy) res.status(403).json(fail(buy));
+    else res.status(200).json(sucess(buy));
+  } catch (err) {
+    res.status(400).json(fail(err.message.split(",\n")));
+  }
 };
 
 exports.get = async (req, res, next) => {
-  let buy = await BuyModel.read();
-  if (!buy) res.status(404).json(fail(buy));
-  else res.status(200).json(sucess(buy));
+  try {
+    let buy = await BuyModel.read();
+    if (!buy) res.status(404).json(fail(buy));
+    else res.status(200).json(sucess(buy));
+  } catch (err) {
+    res.status(400).json(fail(err.message.split(",\n")));
+  }
 };
 
 exports.getByCode = async (req, res, next) => {
-  let buy = await BuyModel.readByCode(
-    req.query.cpf,
-    req.query.code,
-    req.query.date,
-    req.query.time
-  );
-  if (!buy) res.status(404).json(fail(buy));
-  else res.status(200).json(sucess(buy));
+  try {
+    let buy = await BuyModel.readByCode(
+      req.query.cpf,
+      req.query.code,
+      req.query.date,
+      req.query.time
+    );
+    if (!buy) res.status(404).json(fail(buy));
+    else res.status(200).json(sucess(buy));
+  } catch (err) {
+    res.status(400).json(fail(err.message.split(",\n")));
+  }
 };
 
 exports.put = async (req, res, next) => {
-  let buy = await BuyModel.update(
-    req.query.cpf,
-    req.query.code,
-    req.query.date,
-    req.query.time,
-    req.body
-  );
-  if (!buy) res.status(404).json(fail(buy));
-  else res.status(200).json(sucess(buy));
+  try {
+    let buy = await BuyModel.update(
+      req.query.cpf,
+      req.query.code,
+      req.query.date,
+      req.query.time,
+      req.body
+    );
+    if (!buy) res.status(404).json(fail(buy));
+    else res.status(200).json(sucess(buy));
+  } catch (err) {
+    res.status(400).json(fail(err.message.split(",\n")));
+  }
 };
 
 exports.delete = async (req, res, next) => {
-  let buy = await BuyModel.delete(
-    req.query.cpf,
-    req.query.code,
-    req.query.date,
-    req.query.time
-  );
-  if (!buy) res.status(404).json(fail(buy));
-  else res.status(200).json(sucess(buy));
+  try {
+    let buy = await BuyModel.delete(
+      req.query.cpf,
+      req.query.code,
+      req.query.date,
+      req.query.time
+    );
+    if (!buy) res.status(404).json(fail(buy));
+    else res.status(200).json(sucess(buy));
+  } catch (err) {
+    res.status(400).json(fail(err.message.split(",\n")));
+  }
 };
+
+try {
+  // AQUI
+} catch (err) {
+  res.status(400).json(fail(err.message.split(",\n")));
+}
--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -2,21 +2,33 @@ const UserModel = require("../models/user");
 const { sucess, fail } = require("../helpers/response");
 
 exports.post = async (req, res, next) => {
-  let user = await UserModel.create(req.body);
-  if (!user) res.status(403).json(fail(user));
-  else res.status(200).json(sucess(user));
+  try {
+    let user = await UserModel.create(req.body);
+    if (!user) res.status(403).json(fail(user));
+    else res.status(200).json(sucess(user));
+  } catch (err) {
+    res.status(400).json(fail(err.message.split(",\n")));
+  }
 };
 
 exports.get = async (req, res, next) => {
-  let users = await UserModel.read();
-  if (!users) res.status(404).json(fail(users));
-  else res.status(200).json(sucess(users));
+  try {
+    let users = await UserModel.read();
+    if (!users) res.status(404).json(fail(users));
+    else res.status(200).json(sucess(users));
+  } catch (err) {
+    res.status(400).json(fail(err.message.split(",\n")));
+  }
 };
 
 exports.getByCode = async (req, res, next) => {
-  let user = await UserModel.readByPk(req.params.cpf);
-  if (!user) res.status(404).json(fail(user));
-  else res.status(200).json(sucess(user));
+  try {
+    let user = await UserModel.readByPk(req.params.cpf);
+    if (!user) res.status(404).json(fail(user));
+    else res.status(200).json(sucess(user));
+  } catch (err) {
+    res.status(400).json(fail(err.message.split(",\n")));
+  }
 };
 
 exports.getLogin = async (req, res, next) => {
@@ -27,16 +39,24 @@ exports.getLogin = async (req, res, next) => {
   } catch (err) {
     res.status(400).json(fail(err.message.split(",\n")));
   }
-}
+};
 
 exports.put = async (req, res, next) => {
-  let user = await UserModel.update(req.params.cpf, req.body);
-  if (!user) res.status(404).json(fail(user));
-  else res.status(200).json(sucess(user));
+  try {
+    let user = await UserModel.update(req.params.cpf, req.body);
+    if (!user) res.status(404).json(fail(user));
+    else res.status(200).json(sucess(user));
+  } catch (err) {
+    res.status(400).json(fail(err.message.split(",\n")));
+  }
 };
 
 exports.delete = async (req, res, next) => {
-  let user = await UserModel.delete(req.params.cpf);
-  if (!user) res.status(404).json(fail(user));
-  else res.status(200).json(sucess(user));
+  try {
+    let user = await UserModel.delete(req.params.cpf);
+    if (!user) res.status(404).json(fail(user));
+    else res.status(200).json(sucess(user));
+  } catch (err) {
+    res.status(400).json(fail(err.message.split(",\n")));
+  }
 };
